f3a826760c5f4857b3e9d12d14ed176afa41e0ce
Zain Ul Abid <bajwazain121@outlook.com>
2024-08-03 19:15:43 -0400

    Added code for adding notes and deleting codes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -184,7 +184,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
+if (window.location.pathname === './notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   clearBtn.addEventListener('click', renderActiveNote);
